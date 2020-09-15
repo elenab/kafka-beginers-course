38e74a7d30e4bf7fca75026496f47fa9d9425272
Elena Busila <elena.busila@gmail.com>
2020-09-15 15:06:21 -0400

    Java producer callback. Java producer with keys.

---
+++ src/main/java/com/github/elenab/kafka/tutorial1/ProducerDemoKeys.java
@@ -0,0 +1,66 @@
+package com.github.elenab.kafka.tutorial1;
+
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+/**
+ * Created by elenabusila on 2020-09-15.
+ */
+public class ProducerDemoKeys {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+
+        final Logger logger = LoggerFactory.getLogger(ProducerDemoKeys.class);
+
+        String bootstrapServers = "127.0.0.1:9092";
+
+        // create Producer properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // create the producer
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+        for (int i = 0; i < 10; i++) {
+            String topic = "first_topic";
+            String value = "Message with keys " + Integer.toString(i);
+            String key = "id_" + Integer.toString(i);
+
+            // create a producer record
+            ProducerRecord<String, String> record =
+                    new ProducerRecord<String, String>(topic, key, value);
+
+            // send data - asynchronous
+            producer.send(record, new Callback() {
+                public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                    //executes every time a record is successfully sent or an error is thrown
+                    if (e == null) {
+                        // the record was successfully sent
+                        logger.info("\n\nReceived new metadata.\n" +
+                                "Topic: " + recordMetadata.topic() + "\n" +
+                                "Partition: " + recordMetadata.partition() + "\n" +
+                                "Offset: " + recordMetadata.offset() + "\n" +
+                                "Timestamp: " + recordMetadata.timestamp() + "\n\n");
+
+                    } else {
+                        logger.error("Error while producing", e);
+
+                    }
+                }
+            });
+        }
+
+
+        // flush data
+        producer.flush();
+        // flush and close producer
+        producer.close();
+
+    }
+}
